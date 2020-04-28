4fb6c1849df78478ba2cef74656a98b2e177418c
Sean Murray <sean.j.murray1@gmail.com>
2020-04-28 13:54:01 -0700

    Add content to index.html and images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// on load user is asked for their name, name inserted into H1
+var userName = prompt('Hi, what is your name?');
+document.getElementById('userName').innerHTML = userName;
+
